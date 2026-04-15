a55faa336be06f7da8b4eb865c7e66d0d1f5955d
Emeka <emekadike412@gmail.com>
2026-04-15 17:08:23 +0100

    Initial commit

---
+++ TaskOne.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class TaskOne {
+
+public static void main(String[] args) {
+
+
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter scores");
+int scores = input.nextInt();
+
+
+for (int scores = ()-> scores > 10; scores++;
+
+
+sum = scores;
+
+System.out.println(sum);
+
+
+
+}
+}
+}
+
+
