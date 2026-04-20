747a5ba1195e76ce5e7d05e8e4f0f16c75010fd0
Emeka <emekadike412@gmail.com>
2026-04-20 08:48:14 +0100

    Add TOUCHGRASSTWO to main branch

---
+++ TOUCHGRASSTWO/TOUCHGRASSTWO/DivideTwoNumbers.java
@@ -0,0 +1,15 @@
+public class DivideTwoNumbers{
+    public static void main(String... args){
+    
+        System.out.print(divide(10, 21));
+    
+    }
+    
+    public static int divide(int firstNumber, int secondNumber){
+        if (firstNumber > secondNumber){
+            return firstNumber / secondNumber;
+        }else{
+            return secondNumber / firstNumber;
+        }
+    }
+}
